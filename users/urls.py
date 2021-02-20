123e349c84844f364950b1c8fab2d900493f302e
Luis Manuel González <mannyglzg@gmail.com>
2021-02-20 14:43:05 -0800

    Add tweets of user endpoint
    Profile queries were changed and
    get_object_or_404 was used instead
    Custom pagination for UserTweets view was created

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.UserCreate.as_view()),
     path('<slug:username>/', views.UserDetail.as_view()),
+    path('<slug:username>/tweets/', views.UserTweets.as_view()),
     path('<slug:username>/following/', views.FollowingDetail.as_view()),
     path('<slug:username>/followers/', views.FollowerDetail.as_view()),
     path('follow/<int:user_id>/', views.ProfileFollow.as_view())
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404
 from .models import User, Profile
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 from .serializers import (
     UserSerializer,
     UserCreateSerializer,
@@ -6,9 +9,11 @@ from .serializers import (
     FollowersSerializer,
     ProfileFollowSerializer
 )
-from rest_framework import generics, status, permissions
-from rest_framework.response import Response
 from .permissions import IsOwnerOrReadOnly
+from .pagination import ShortResultsSetPagination
+from tweets.serializers import TweetSerializer
+from tweets.models import Tweet
+from users.models import User
 
 
 class UserCreate(generics.CreateAPIView):
@@ -22,6 +27,21 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'username'
+
+
+class UserTweets(generics.ListAPIView):
+    serializer_class = TweetSerializer
+    lookup_field = 'username'
+    pagination_class = ShortResultsSetPagination
+
+    def get_queryset(self):
+        """
+        This view return a list of all the tweets
+        of the user.
+        """
+        username = self.kwargs.get('username')
+        user = get_object_or_404(User, username=username)
+        return Tweet.objects.filter(user=user)
 
 
 class FollowingDetail(generics.RetrieveAPIView):
@@ -45,8 +65,8 @@ class ProfileFollow(generics.RetrieveUpdateAPIView):
     def put(self, request, *args, **kwargs):
         user_id = kwargs.get('user_id')  # get lookup_field
 
-        follower_profile = Profile.objects.get(user=request.user)
-        followed_profile = Profile.objects.get(pk=user_id)
+        follower_profile = get_object_or_404(Profile, user=request.user)
+        followed_profile = get_object_or_404(Profile, pk=user_id)
 
         # check if followed profile exists in follower profile
         if follower_profile.following.filter(pk=user_id).exists():
